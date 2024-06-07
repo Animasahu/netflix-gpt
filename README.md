184dc2cc0212574f4df247c186cd6bebb4f46e72
Anima Sahu <animasahu2009@gmail.com>
2024-06-08 01:12:34 +0530

    Validate the form data

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Netflic GPT
+- Create react app
+- Configured TailwindCSS
+- Header
+- Routing of APP
+- Login Form
+- Sign up Form
+- Form Validation
+
+
+
 # Features
 - Login/Sign Up
   - Sign In/Sign up Form
@@ -12,3 +23,7 @@
 - NetflixGPT
    - Search Bar
    - Movie Suggestions 
+
+
+   # How do you do validation inside a form - formik library
+   # useRef is used to refernce a field over here, so basically tag over here suppose input tag , a button these gives us reference to it.
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //validate the form data
+
+    // console.log(email.current.value);
+    // console.log(password.current.value);
+
+    const message  = checkValidData(name.current.value , email.current.value , password.current.value);
+    setErrorMessage(message);
+
+    //sign / sign up
+  };
 
   const toggleSignInForm = () => {
     setSignInForm(!isSignInForm);
@@ -17,29 +35,44 @@ const Login = () => {
         />
       </div>
 
-      <form className=" w-3/12 absolute p-12 bg-black my-24 mx-auto right-0 left-0 text-white opacity-4 rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        {!isSignInForm && (<input
-          type="text"
-          placeholder="Full name"
-          className="p-4 my-2 w-full bg-gray-600"
-        />
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className=" w-3/12 absolute p-12 bg-black my-24 mx-auto right-0 left-0 text-white opacity-4 rounded-lg bg-opacity-70"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref = {name}
+            type="text"
+            placeholder="Full name"
+            className="p-4 my-2 w-full bg-gray-600"
+          />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email or mobile number"
           className="p-4 my-2 w-full bg-gray-600"
         />
         <input
+          ref={password}
           type="text"
           placeholder="Password"
           className="p-4 my-2 w-full bg-gray-600"
         />
-        <button className="p-3 my-6 bg-red-700 w-full rounded-md cursor-pointer">
-           {isSignInForm ? "Sign In" : "Sign Up"}
+        <p className="text-red-500 font-bold"> {errorMessage} </p>
+        <button
+          className="p-3 my-6 bg-red-700 w-full rounded-md cursor-pointer"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm ? "New to Netflix? Sign up now." : "Already registered? Sign In now."}
+          {isSignInForm
+            ? "New to Netflix? Sign up now."
+            : "Already registered? Sign In now."}
         </p>
       </form>
     </div>
